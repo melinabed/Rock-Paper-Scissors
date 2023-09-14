d1284a4080783d2ca93f133bf7bc00d4fe5124cc
melinabed <melo36049@hotmail.com>
2023-09-14 13:21:56 -1000

    revised but game function not working

--- rps.js
+++ rps.js
@@ -1,50 +1,45 @@
-//My Variables
-const choices = ["rock","paper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
+const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-let computerScore = 0;
+const score = playRound();
+
 let playerScore = 0;
-let playerSelection = prompt("Do you chooose rock, paper, or scissors?");
-    if (playerSelection.toLowerCase());
+let computerScore = 0;
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
+function game(playerScore, computerScore) {
+    let finalResult;
+    for (let score = 0; score <= 5; score++) {
+        if (playerScore == 5) {
+        finalResult = "Player won the game"
+        } else if (computerScore == 5) {
+        finalResult = "Computer won the game"
+        }
+        }
+        return finalResult;
 }
 
-//Lets Game Loop 5 times.
-function game() {
-    for (let i = 0; i < 5; i++) {
-        score()
-    }
-}
-//Keeps Score and declares winner overall.
-function score() { 
-    playRound(playerSelection,computerSelection);
-    if (playerScore>computerScore){
-        console.log("Player Won");
-    }
-    if (computerScore>playerScore) {
-        console.log("Computer Won");
-    }
-}
-//Defines the variables of what wins    
+function getComputerChoice () {
+    return choices[Math.floor(Math.random() * 3)];
+}   
+
+
 function playRound(playerSelection, computerSelection) {
     let result;
     if (playerSelection === computerSelection) {
         result = "Tie!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-               playerSelection === "paper" && computerSelection === "rock" ||
-               playerSelection === "scissors" && computerSelection === "paper"){
-        result = "You Win!";
-        playerScore += 1;   
-        } else (computerSelection === "rock" && playerSelection === "scissors" ||
-                computerSelection === "paper" && playerSelection === "rock" ||
-                computerSelection === "scissors" && playerSelection === "paper"); {
-        result = "You Lose!";
-        computerScore += 1;
-    return result;}
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+                (playerSelection === "scissors" && computerSelection === "paper") ||
+                (playerSelection === "paper" && computerSelection === "rock")) {
+                playerScore = ++playerScore;
+                result = "Player Wins";
+    } else if  ((computerSelection === "rock" && playerSelection === "scissors") ||
+                (computerSelection === "scissors" && playerSelection === "paper") ||
+                (computerSelection === "paper" && playerSelection === "rock")) {
+                computerScore = ++computerScore;
+                result = "Computer Wins";
+        }
+        return result;
 }
 
-
+console.log(game(playerScore, computerScore));
 console.log(playRound(playerSelection, computerSelection));
-
-
