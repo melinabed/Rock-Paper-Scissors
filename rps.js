83475941d8a0a36aecf25344199fee5e9a3c116e
melinabed <melo36049@hotmail.com>
2023-09-19 10:31:16 -1000

    added css

--- rps.js
+++ rps.js
@@ -1,86 +1,59 @@
-const choices = ["rock", "paper", "scissors"];
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-//Gets a random choice between rock paper, or scissors for the computer.
-function getComputerChoice() {
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    return computerChoice;
-}   
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
-//Determines the winner via choices for player and computer.
-function checkWinner(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "Tie";
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Maybe there is hope for humanity. Reload the page to play again.'
+            disableButtons()
+        }
     }
-    else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-    ){
-        return "Player";
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
     else {
-        return "Computer";
-    }
-}
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
-//Determines the result of the choices for the player and computer.
-function playRound(playerSelection, computerSelection) {
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == "Tie") {
-        return "It's a Tie! Come on dude!";
-    }
-    else if (result == "Player") {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
-    }
-    else if (result == "Computer") {
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
-    }
-}
-
-//Develops the prompt for the player to enter and determines makes the input case-insensitive.
-function getplayerChoice() {
-    let validatedInput = false;
-    while (validatedInput == false) {
-        const playerOption = prompt("Do you choose Rock, Paper, or Scissors?");
-        if (playerOption == null) {
-            continue;
-        }
-        const optionInLower = playerOption.toLowerCase();
-        if (choices.includes(optionInLower)) {
-            validatedInput = true;
-            return optionInLower;
+        if (computerScore == 5) {
+            result += '<br><br>Computer won the game! AI will take over one day.. Reload the page to play again.'
+            disableButtons()
         }
     }
+
+    document.getElementById('result').innerHTML = result
+    return
 }
 
-//Uses the checkWinner function and loops 5 times.
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getplayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if (checkWinner(playerSelection, computerSelection) == "Player") {
-            playerScore++
-        }
-        else if (checkWinner(playerSelection, computerSelection) == "Computer")
-            computerScore++
-    }
-    console.log("Game Over, Refresh to play again.")
-    if (playerScore > computerScore) {
-        console.log("I guess you win");
-    }
-    else if (computerScore > playerScore) {
-        console.log("Computer won, what a doofus. AI will take over one day...");
-    }
-    else {
-        console.log("Tie Game! Not smart enough to win are you?");
-    }
-}
-
-//Calls the game.
-game();
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
 
 
 
